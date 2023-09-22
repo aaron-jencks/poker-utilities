5e67335ad868189e309bf354cd332e7e2b854737
Aaron Jencks <aaron.jencks@syncbak.com>
2023-09-22 10:34:15 -0500

    testing card code

--- card/card_test.go
+++ card/card_test.go
@@ -1,6 +1,8 @@
 package card
 
 import (
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,5 +20,19 @@ func TestParseCardString(t *testing.T) {
 
 	for _, tc := range tcs {
 		assert.Equal(t, tc.c, ParsePokerCardString(tc.s))
+		assert.Equal(t, tc.s, ParsePokerCardString(tc.s).String())
+	}
+}
+
+func TestSortingCards(t *testing.T) {
+	cards := []string{"2c", "7h", "as"}
+	ocards := make([]Card, len(cards))
+	for ci, c := range cards {
+		ocards[ci] = ParsePokerCardString(c)
+	}
+	rand.Shuffle(len(ocards), func(i, j int) { ocards[i], ocards[j] = ocards[j], ocards[i] })
+	sort.Slice(ocards, func(i, j int) bool { return ocards[i].LessThan(ocards[j]) })
+	for ci := range cards {
+		assert.Equal(t, cards[ci], ocards[ci].String())
 	}
 }
