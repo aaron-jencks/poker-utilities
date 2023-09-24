2811e6ad7232a1aaf16b2bfa5f8a3bf2d0f0fca8
Aaron Jencks <aaronjencks@aol.com>
2023-09-24 00:19:49 -0500

    adding poker ranges

--- hand/range.go
+++ hand/range.go
@@ -7,3 +7,26 @@ type PokerRange struct {
 	F1     card.CardFace
 	Suited bool
 }
+
+func (r PokerRange) Pairs() [][]card.Card {
+	results := [][]card.Card{}
+	for s := card.CLUBS; s < card.SUITS; s++ {
+		c0 := card.CreateCard(r.F0, s)
+
+		if r.Suited {
+			c1 := card.CreateCard(r.F1, s)
+			results = append(results, []card.Card{c0, c1})
+			continue
+		}
+
+		for s1 := card.CLUBS; s1 < card.SUITS; s1++ {
+			if s == s1 {
+				continue
+			}
+
+			c1 := card.CreateCard(r.F1, s1)
+			results = append(results, []card.Card{c0, c1})
+		}
+	}
+	return results
+}
---
+++ simulation/range.go
@@ -0,0 +1,9 @@
+package simulation
+
+import (
+	"github.com/aaron-jencks/poker/hand"
+)
+
+func SimulateTableRangeHand(nplayers int, r hand.PokerRange, folds map[int]bool) [][]int {
+	return nil
+}
